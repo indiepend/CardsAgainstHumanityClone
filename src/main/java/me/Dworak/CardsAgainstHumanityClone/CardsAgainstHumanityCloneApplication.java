de7c3a30a18a88fbd7ae260fad36008e3b7c1b72
Robert <robert.p.dworak@gmail.com>
2021-12-11 03:08:42 +0100

    add files

---
+++ src/main/java/me/Dworak/CardsAgainstHumanityClone/CardsAgainstHumanityCloneApplication.java
@@ -0,0 +1,13 @@
+package me.Dworak.CardsAgainstHumanityClone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardsAgainstHumanityCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardsAgainstHumanityCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/Dworak/CardsAgainstHumanityClone/CardsAgainstHumanityCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package me.Dworak.CardsAgainstHumanityClone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardsAgainstHumanityCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
